70221127be87bc877714c4cf54ed518e7b5f0d08
Alumno <hugogmartin@hotmail.com>
2025-04-22 21:29:33 +0200

    Revert "prueba de que solo se sube el src"
    This reverts commit eaba8231081c59d283a6a236027bc5e5a984a644.

--- src/lsi/ubu/servicios/ServicioImpl.java
+++ src/lsi/ubu/servicios/ServicioImpl.java
@@ -42,7 +42,7 @@ public class ServicioImpl implements Servicio {
 
 			/* A completar por el alumnado... */
 
-			/* ================================= AYUDA Rï¿½PIDA ===========================*/
+			/* ================================= AYUDA RÁPIDA ===========================*/
 			/*
 			 * Algunas de las columnas utilizan tipo numeric en SQL, lo que se traduce en
 			 * BigDecimal para Java.
